3307b4711dc616d2b4671a4b382d0688cf795a23
Txema Bermudez <josemaria.bermudez@idoven.ai>
2022-12-01 16:54:14 +0100

    01-I

---
+++ day01.py
@@ -0,0 +1,13 @@
+file = open("day01_input.txt", "r")
+lines = file.read().splitlines()
+
+elves = []
+current = 0
+for line in lines:
+    if line != "":
+        current += int(line)
+    else:
+        elves.append(current)
+        current = 0
+
+print(max(elves))
