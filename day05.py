2e56549aac75139147241e8fd49288cce2216481
Txema Bermudez <josemaria.bermudez@idoven.ai>
2022-12-05 16:59:27 +0100

    05-II

--- day05.py
+++ day05.py
@@ -24,12 +24,12 @@
 # stacks = [["Z", "N"], ["M", "C", "D"], ["P"]]
 
 file = open("day05_input.txt", "r")
-input = file.read().splitlines()
+input = [line for line in file.readlines() if line.startswith("move")]
 file.close()
 
 stacks = [
     ["J", "H", "G", "M", "Z", "N", "T", "F"],
-    ["J", "W", "V"],
+    ["V", "W", "J"],
     ["G", "V", "L", "J", "B", "T", "H"],
     ["B", "P", "J", "N", "C", "D", "V", "L"],
     ["F", "W", "S", "M", "P", "R", "G"],
@@ -42,10 +42,14 @@ stacks = [
 for line in input:
     split_line = line.split()
     num = int(split_line[1])
-    stack_from = int(split_line[3]) - 1
-    stack_to = int(split_line[5]) - 1
+    # stack_from = int(split_line[3]) - 1
+    # stack_to = int(split_line[5]) - 1
+    stack_from = stacks[int(split_line[3]) - 1]
+    stack_to = stacks[int(split_line[5]) - 1]
 
+    stack_to += stack_from[len(stack_from) - num:]
     for _ in range(num):
-        stacks[stack_to].append(stacks[stack_from].pop())
+        # stacks[stack_to].append(stacks[stack_from].pop())
+        stack_from.pop()
 
 print("".join([x.pop() for x in stacks]))
