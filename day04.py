cf83866969d466357096608a8488523d3cbb810b
Txema Bermudez <josemaria.bermudez@idoven.ai>
2022-12-05 13:23:14 +0100

    04-II

--- day04.py
+++ day04.py
@@ -9,11 +9,21 @@ file = open("day04_input.txt", "r")
 input = file.read().splitlines()
 file.close()
 
+# count = 0
+# for line in input:
+#     elf1_start, elf1_end, elf2_start, elf2_end = [int(x) for elfs in line.split(",") for x in elfs.split("-")]
+#     elf1 = list(range(elf1_start, elf1_end + 1))
+#     elf2 = list(range(elf2_start, elf2_end + 1))
+#     count += 1 if set(elf1).issubset(elf2) or set(elf2).issubset(elf1) else 0
+#
+# print(count)
+
+
 count = 0
 for line in input:
     elf1_start, elf1_end, elf2_start, elf2_end = [int(x) for elfs in line.split(",") for x in elfs.split("-")]
     elf1 = list(range(elf1_start, elf1_end + 1))
     elf2 = list(range(elf2_start, elf2_end + 1))
-    count += 1 if set(elf1).issubset(elf2) or set(elf2).issubset(elf1) else 0
+    count += 1 if set(elf1) & set(elf2) else 0
 
 print(count)
