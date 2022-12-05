2ca8396232c9bf87bf7b6d41f2404a66fe2e9e98
Txema Bermudez <josemaria.bermudez@idoven.ai>
2022-12-05 12:56:59 +0100

    03-II

--- day03.py
+++ day03.py
@@ -5,18 +5,28 @@ import string
 # PmmdzqPrVvPwwTWBwg
 # wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 # ttgJtRGJQctTZtZT
-# CrZsJsPPZsGzwwsLwLmpwMDw"""
+# CrZsJsPPZsGzwwsLwLmpwMDw""".split("\n")
 
 file = open("day03_input.txt", "r")
 input = file.read().splitlines()
 file.close()
 
 score = string.ascii_lowercase + string.ascii_uppercase
+#
+# result = 0
+# for line in input:
+#     index = len(line) // 2
+#     item_shared = list(set(line[:index]) & set(line[index:]))[0]
+#     result += score.index(item_shared) + 1
+#
+# print(result)
 
 result = 0
-for line in input:
-    index = len(line) // 2
-    item_shared = list(set(line[:index]) & set(line[index:]))[0]
+lines = iter(input)
+for line1 in lines:
+    line2 = next(lines)
+    line3 = next(lines)
+    item_shared = list(set(line1) & set(line2) & set(line3))[0]
     result += score.index(item_shared) + 1
 
 print(result)
