46416a5366af8aa91e5015fc5836831df8ae7649
Txema Bermudez <josemaria.bermudez@idoven.ai>
2022-12-06 19:49:00 +0100

    06-II

--- day06.py
+++ day06.py
@@ -8,7 +8,7 @@ file = open("day06_input.txt", "r")
 input = file.readline()
 file.close()
 
-for i in range(len(input) - 3):
-    if len(set(input[i:i + 4])) == 4:
-        print(i + 4)
+for i in range(len(input) - 13):
+    if len(set(input[i:i + 14])) == 14:
+        print(i + 14)
         break
